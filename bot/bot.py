6b8d6a2608e6881665921931b66ed1cd339967f3
yklas <114969203+yklas@users.noreply.github.com>
2025-02-17 21:29:05 +0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -571,5 +571,13 @@ async def main():
         await bot.session.close()
         scheduler.shutdown()
 
+async def main():
+    scheduler.start()
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
 if __name__ == "__main__":
     asyncio.run(main())
