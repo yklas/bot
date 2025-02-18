120e6e17bed7e8e0930d4c4506717bdb5e86785a
yklas <114969203+yklas@users.noreply.github.com>
2025-02-18 15:34:11 +0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -89,7 +89,7 @@ GROUP_MESSAGES = {
 english_schedule = [
     {'hour': 9, 'minute': 0},
     {'hour': 14, 'minute': 47},
-    {'hour': 17, 'minute': 0},
+    {'hour': 15, 'minute': 36},
     {'hour': 21, 'minute': 0}
 ]
 
